b3a17be8686af55dad1538573b232ee993c945d3
RobertJaro <robert.jarolim@gmail.com>
2021-10-03 22:04:14 +0200

    update install

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     license='GPL-3.0',
     author='Robert Jarolim',
     description='Package for translation between image domains of different astrophysical instruments.',
-    #install_requires=['torch>=1.8', 'sunpy>=2.0', 'scikit-image', 'scikit-learn', 'tqdm', 'numpy', 'matplotlib', 'astropy', 'aiapy'],
+    install_requires=['torch>=1.8', 'sunpy>=2.0', 'scikit-image', 'scikit-learn', 'tqdm',
+                      'numpy', 'matplotlib', 'astropy', 'aiapy','drms'],
     classifiers=[
     'Development Status :: 3 - Alpha', # either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'License :: OSI Approved :: GPL-3.0 License',
