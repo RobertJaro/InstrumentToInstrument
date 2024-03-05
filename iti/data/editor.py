045a4e2ec059cb2481ac30c1a42625e86b9138c3
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-03-05 20:46:23 +0100

    Updated NormalizeRadiusEditor to adjust irradiance with distance

--- iti/data/editor.py
+++ iti/data/editor.py
@@ -334,43 +334,37 @@ class NormalizeRadiusEditor(Editor):
     def call(self, s_map, **kwargs):
         warnings.simplefilter("ignore")  # ignore warnings
 
-        if self.fix_irradiance_with_distance:
-            old_meta = s_map.meta.copy()
+        original_map = s_map
+
         r_obs_pix = s_map.rsun_obs / s_map.scale[0]  # Get the solar radius in pixels
-        r_obs_pix = (1 + self.padding_factor) * r_obs_pix  # Get the size in pixels of the padded radius 
+        r_obs_pix = (1 + self.padding_factor) * r_obs_pix  # Get the size in pixels of the padded radius
         scale_factor = self.resolution / (2 * r_obs_pix.value)
         s_map = Map(np.nan_to_num(s_map.data).astype(np.float32), s_map.meta)
-        s_map = s_map.rotate(recenter=True, scale=scale_factor, missing=0, order=4)
         if self.rotate_north_up:
             s_map = s_map.rotate(recenter=True, scale=scale_factor, missing=0, order=4)
         else:
             s_map = s_map.rotate(angle=0*u.deg, recenter=False, scale=scale_factor, missing=0, order=4)
         if self.crop:
             arcs_frame = (self.resolution / 2) * s_map.scale[0].value
-            s_map = s_map.submap(bottom_left=SkyCoord(-arcs_frame * u.arcsec, -arcs_frame * u.arcsec, frame=s_map.coordinate_frame),
-                                 top_right=SkyCoord(arcs_frame * u.arcsec, arcs_frame * u.arcsec, frame=s_map.coordinate_frame))
+            s_map = s_map.submap(
+                bottom_left=SkyCoord(-arcs_frame * u.arcsec, -arcs_frame * u.arcsec, frame=s_map.coordinate_frame),
+                top_right=SkyCoord(arcs_frame * u.arcsec, arcs_frame * u.arcsec, frame=s_map.coordinate_frame))
             pad_x = s_map.data.shape[0] - self.resolution
             pad_y = s_map.data.shape[1] - self.resolution
             s_map = s_map.submap(bottom_left=[pad_x // 2, pad_y // 2] * u.pix,
                                  top_right=[pad_x // 2 + self.resolution - 1, pad_y // 2 + self.resolution - 1] * u.pix)
-        
+
         s_map.meta['r_sun'] = s_map.rsun_obs.value / s_map.meta['cdelt1']
 
         # Virtually move the instrument such that the sun occupies the expected
         # size in the current optics
         if self.fix_irradiance_with_distance:
-            # The sun is bigger, not the scaling of the detector
-            s_map.meta['rsun_obs'] = old_meta['rsun_obs']*scale_factor
-            # This means that cdelt is the same as the old one
-            s_map.meta['cdelt1'] = old_meta['cdelt1']
-            s_map.meta['cdelt2'] = old_meta['cdelt1']           
-            # But we are also closer to the sun
-            s_map.meta['dsun_obs'] = (s_map.meta['rsun_ref']/np.tan(s_map.meta['rsun_obs']*u.arcsec)).value
-            # Change intensity due to distance change
-            s_map.data[:] = s_map.data[:] * (old_meta['dsun_obs']**2)/(s_map.meta['dsun_obs']**2)
+            # preserve total intensity at 1 AU
+            s_map.data[:] = s_map.data * (original_map.data.sum() / s_map.data.sum()) * (
+                        original_map.dsun.to_value(u.AU) / 1) ** 2
+            # set radius to 1 AU
+            s_map.meta['dsun_obs'] = (1 * u.AU).to_value(u.m)
 
-        if self.scale_irradiance_to_1AU:
-            s_map.data[:] = s_map.data[:] * (s_map.meta['dsun_obs'] / sunpy.sun.constants.au.value) ** 2
         return s_map
 
 
