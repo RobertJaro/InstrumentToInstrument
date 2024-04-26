892f1ba51929f55ee7d9b20918ef2ec4168213f1
RobertJaro <robert.jarolim@gmail.com>
2024-04-26 16:34:46 +0200

    update setup, add requirements

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ import setuptools
 from setuptools import setup, find_packages
 
 setup(
-    name='InstrumentToInstrument',
+    name='iti',
     version='0.1',
     packages=setuptools.find_packages(),
     url='https://github.com/RobertJaro/InstrumentToInstrument',
-    download_url='',
     license='GPL-3.0',
     author='Robert Jarolim',
     description='Package for translation between image domains of different astrophysical instruments.',
     install_requires=['torch>=1.8', 'sunpy>=2.0', 'scikit-image', 'scikit-learn', 'tqdm',
                       'numpy', 'matplotlib', 'astropy', 'aiapy','drms'],
     classifiers=[
-    'Development Status :: 3 - Alpha', # either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Development Status :: 4 - Beta', # either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'License :: OSI Approved :: GPL-3.0 License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.9',
   ],
 )
