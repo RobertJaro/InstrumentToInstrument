1f54c24181931831d6d0e8055634ae82ac4f6e11
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-05-17 11:53:24 +0200

    update to download public available buckets

--- iti/evaluation/evaluation.py
+++ iti/evaluation/evaluation.py
@@ -123,7 +123,7 @@ def saveToFITS(maps, path):
 
 ################################### Download GCP bucket ###################################
 
-def download_bucket_with_transfer_manager(bucket_name, destination_directory="", workers=8, max_results=1000):
+def download_gcp_bucket(bucket_name, destination_directory="", workers=8, max_results=1000):
 
     """Download all of the blobs in a bucket, concurrently in a process pool.
 
